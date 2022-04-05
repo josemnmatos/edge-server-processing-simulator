a6f92600efa8e02096fa45cfdfca977ce3345dd8
José Matos <josemnmatos@gmail.com>
2022-04-05 14:48:32 +0100

    14:48

--- main.c
+++ main.c
@@ -6,7 +6,14 @@
 #include <signal.h>
 #include <fcntl.h>
 
+#define PIPE_NAME "TASK_PIPE"
+#define NUM_EDGESERVERS_THREADS 2
 
+void *system_manager(void *p);
+void *task_manager(void *p);
+void *edge_server(void *p);
+void *monitor(void *p);
+void *maintenance_manager(void *p);
 
 int main(int argc, char const *argv[])
 {
